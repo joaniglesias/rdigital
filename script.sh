dd49ee157ec5a8b64284c8fcb42235a2258f88aa
Joan <Joan.IglesiasRomaguera@vimn.com>
2014-06-17 08:15:34 +0100

    update script.sh for testing

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /var/www-shared/test/rdigital
-chmod -R 777 rdigital-web
-sudo rm -rf rdigital-web
-drush make distro.make rdigital-web --working-copy --no-gitinfofile
-cd /var/www-shared/test/rdigital/rdigital-web
+chmod -R 777 platform_2014_testing
+sudo rm -rf platform_2014_testing
+drush make distro.make platform_2014_testing --no-gitinfofile --working-copy
+cd /var/www-shared/test/rdigital/platform_2014_testing
 drush si rdigital --db-url='mysql://root:root@localhost/rdigital' --site-name='R Digital Site' --account-pass=admin -y
 cd /var/www-shared/test/rdigital
-chmod 777 rdigital-web/sites/default/files/ -R
+chmod 777 files/ -R
