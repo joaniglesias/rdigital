b2af6ce76383054c2d542939bf9a349274cd8f79
Joan <Joan.IglesiasRomaguera@vimn.com>
2014-08-03 07:45:13 +0100

    change script's code to point to uat site

--- db_script.sh
+++ db_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /var/www-shared/test/rdigital/assets
+cd /var/www-shared/test/rdigital/uat/assets
 mysqldump -u root -proot -h localhost drupalm1_rdigital --lock-tables=false > drupalm1_rdigital.mysql
--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd /var/www-shared/test/rdigital
+cd /var/www-shared/test/rdigital/uat
 chmod -R 777 platform_2014_testing
 sudo rm -rf platform_2014_testing
 rm public_html
 drush make distro.make platform_2014_testing --no-gitinfofile --working-copy
-cd /var/www-shared/test/rdigital/platform_2014_testing
-drush si rdigital --db-url='mysql://root:root@localhost/rdigital' --site-name='R Digital Site' --account-pass=admin -y
-cd /var/www-shared/test/rdigital
-ln -s /var/www-shared/test/rdigital/platform_2014_testing public_html
+cd /var/www-shared/test/rdigital/uat/platform_2014_testing
+drush si rdigital --db-url='mysql://root:root@localhost/rdigital_uat' --site-name='R Digital Site' --account-pass=admin -y
+cd /var/www-shared/test/rdigital/uat
+ln -s /var/www-shared/test/rdigital/uat/platform_2014_testing public_html
 chmod 777 files/ -R
