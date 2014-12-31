4a9822e4a895bb8945bb60d629071f0f742d7a7c
Joan Iglesias Romaguera <Joan.IglesiasRomaguera@vimn.com>
2014-12-31 16:14:54 +1100

    update script file

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /var/www-shared/test/rdigital/uat
-chmod -R 777 platform_2014_testing
-sudo rm -rf platform_2014_testing
+cd /var/www-shared/test/rdigital/dev
+chmod -R 777 platform_2015
+sudo rm -rf platform_2015
 rm public_html
-drush make distro.make platform_2014_testing --no-gitinfofile --working-copy
-ln -s /var/www-shared/test/rdigital/uat/files platform_2014_testing/sites/default/files
-cd /var/www-shared/test/rdigital/uat/platform_2014_testing
-drush si rdigital --db-url='mysql://root:root@localhost/rdigital_uat' --site-name='R Digital Site' --account-pass=admin -y
-cd /var/www-shared/test/rdigital/uat
-ln -s /var/www-shared/test/rdigital/uat/platform_2014_testing public_html
+drush make distro.make platform_2015 --no-gitinfofile --working-copy
+ln -s /var/www-shared/test/rdigital/dev/files platform_2015/sites/default/files
+cd /var/www-shared/test/rdigital/dev/platform_2015
+drush si rdigital --db-url='mysql://root:root@localhost/rdigital_dev' --site-name='R Digital Site' --account-pass=admin -y
+cd /var/www-shared/test/rdigital/dev
+ln -s /var/www-shared/test/rdigital/dev/platform_2015 public_html
 chmod 777 files/ -R
