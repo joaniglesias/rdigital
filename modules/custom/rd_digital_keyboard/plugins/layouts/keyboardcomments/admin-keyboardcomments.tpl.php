c6d24bb7319ae70ed62ba8d510226ffc97df6ed9
Joan <joaniglesias@gmail.com>
2015-09-05 01:43:49 +0000

    create layout keyboardcomments

---
+++ modules/custom/rd_digital_keyboard/plugins/layouts/keyboardcomments/admin-keyboardcomments.tpl.php
@@ -0,0 +1,18 @@
+<div class="container">
+  <div class="full-size position">
+    <?php print $content['row_1']; ?>
+  </div>
+  <div class="full-size position">
+    <?php print $content['row_2']; ?>
+  </div>
+  <div class="full-size position">
+    <?php print $content['row_3']; ?>
+  </div>
+  <div class="full-size position">
+    <?php print $content['row_4']; ?>
+  </div>
+  <div class="full-size position">
+    <?php print $content['comments']; ?>
+  </div>
+</div>
+
---
+++ modules/custom/rd_digital_keyboard/plugins/layouts/keyboardcomments/keyboardcomments.tpl.php
@@ -0,0 +1,37 @@
+<!-- Row 1 Region -->
+<div class="row">
+  <div class="col-xs-12">
+    <?php print $content['row_1']; ?>
+  </div>
+</div>
+
+<!-- Row 2 Region -->
+<div class="row">
+  <div class="col-xs-12">
+    <?php print $content['row_2']; ?>
+  </div>
+</div>
+
+<!-- Row 3 Region -->
+<div class="row">
+  <div class="col-xs-12">
+    <?php print $content['row_3']; ?>
+  </div>
+</div>
+
+<!-- Row 4 Region -->
+<div class="row">
+  <div class="col-xs-12">
+    <?php print $content['row_4']; ?>
+  </div>
+</div>
+
+<!-- Comments Region -->
+<div class="row">
+  <div class="col-xs-12">
+    <div class="collapse alert alert-danger fade" id="KeyboardComments">
+      <a href="#KeyboardComments" class="close" data-toggle="collapse" aria-label="close" aria-expanded="false" >&times;</a>
+      <?php print $content['comments']; ?>
+    </div>
+  </div>
+</div>
