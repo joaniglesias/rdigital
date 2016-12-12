6589414561a5ea78ebf40db832cd025f435d37ea
Joan <labjoan@gmail.com>
2016-12-13 00:04:08 +1100

    try to fix CKEditor behaviour after clicking Save button - didn't have success :(

--- modules/custom/rd_digital_keyboard/js/button.js
+++ modules/custom/rd_digital_keyboard/js/button.js
@@ -3,7 +3,7 @@
     writetext();
   });
 
-console.log('hola');
+console.log('hola2');
 
 //   jQuery("a.vm-icon-button").on('click', function() {
 //     console.log('adios');
@@ -27,6 +27,7 @@ console.log('hola');
           var ckeditor_id = $('div#' + layout_area_id + ' textarea').attr('id');
           var ckeditor_text = CKEDITOR.instances[ckeditor_id].getData();
           var button_text = $(this).attr('text-to-print');
+
           //insert text with no <p> tag http://ckeditor.com/forums/CKEditor/Jquery-data-insert-into-editor
           //Background color: http://jsbin.com/osohak/7/edit?html,js,output
           //console.log(content);
@@ -35,10 +36,20 @@ console.log('hola');
    console.log( ckeditor_text);
    console.log( ckeditor_text.slice(0,-4));
    console.log( ckeditor_text.slice(0,-5));
-//}
+
           //CKEDITOR.instances[ckeditor_id].insertHtml('<p></p>');
-          CKEDITOR.instances[ckeditor_id].insertHtml(button_text);
           //CKEDITOR.instances[ckeditor_id].insertText('\rhola');
+
+
+          // Insert text where the cursor is placed - CONS: after click Save, cursor goes at the start of the text instead of the end
+          CKEDITOR.instances[ckeditor_id].insertHtml(button_text);
+
+          // Insert text at the end of the text INDEPENDENTLY od where the sursor is placed - CONS: it doesn't respect line breaks
+          // CKEDITOR.instances[ckeditor_id].setData(ckeditor_text.slice(0,-5) + button_text, function() {
+          //     [ckeditor_id].focus();
+          // });
+
+
 //           CKEDITOR.instances[ckeditor_id].setData(ckeditor_text.slice(0,-5) + button_text, function() {
 //     [ckeditor_id].focus();
 // });
@@ -49,11 +60,26 @@ console.log('hola');
       });
 
 
+/*
+      // Edit link: ajax load the form into the modal
+      $('button.form-submit').once('vimn-twitter', function() {
+        $(this).click(function() {
+
+          var layout_area_id = $(this).parents('.panel-body').attr('id');
+
+
+          if (layout_area_id == 'measures-area') {
+            measures_saved = true;
+          }
+          console.log( 'clico boto Saved');
+
+        });
+      });
+*/
+
+
     }
   };
-
-
-
 
 
 
